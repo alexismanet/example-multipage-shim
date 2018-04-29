1fcbc4a93942b3575af6b2ab496b2e0f3a4a7c51
tengma <tengma.chine@gmail.com>
2018-04-29 18:39:35 +0200

    update  bower_components lib

--- app/js/src/common.js
+++ app/js/src/common.js
@@ -1,19 +1,8 @@
-//The build will inline common dependencies into this file.
-
-//For any third party dependencies, like jQuery, place them in the lib folder.
-
-//Configure loading modules from the lib directory,
-//except for 'app' ones, which are in a sibling
-//directory.
 requirejs.config({
-    // baseUrl: 'js/lib',
-    // baseUrl: 'lib',
     paths: {
-
-        backbone : 'backbone',
-        // jquery : 'jquery',
-        jquery : 'jquery/dist/jquery',
-        underscore : 'underscore',
+        backbone: 'backbone/backbone',
+        jquery: 'jquery/dist/jquery',
+        underscore: 'underscore/underscore',
         app: '../app'
     },
     shim: {
